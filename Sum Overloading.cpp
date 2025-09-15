6c31aff68bb75d460b47ed56e92d6a61c5f56bad
KassArc <ayushkumaryadav2029@gmail.com>
2025-09-16 01:49:40 +0530

    Create Sum Overloading.cpp

---
+++ Sum Overloading.cpp
@@ -0,0 +1,38 @@
+// Ayush Yadav
+// 24070123028
+
+#include <iostream>
+using namespace std;
+
+    class addition{
+    public:
+
+int sum(int a , int b ){
+        return a+b;
+       
+    }
+int sum(int a , int b , int c){
+        return a+b+c;
+       
+    }
+    
+double sum(double d , double f ){
+        return d+f;
+       
+    }
+   
+};
+int main(){
+
+    addition obj;
+    cout<< obj.sum(10,20)<<endl;
+    cout<< obj.sum(10,20,20)<<endl;
+    cout<< obj.sum(10.8,20.34)<<endl;
+
+    return 0;
+}
+
+// OUTPUT
+// 30
+// 50
+// 31.14
